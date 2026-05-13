c07e833195fa383f4897a3760abf908a2f20e8af
jade <syeong8663@gmail.com>
2026-05-14 01:08:16 +0900

    fix: 드롭다운API 비로그인 상태에서 접근

--- src/main/java/com/union/demo/config/SecurityConfig.java
+++ src/main/java/com/union/demo/config/SecurityConfig.java
@@ -114,7 +114,7 @@ public class SecurityConfig {
                                 "/api/auth/refresh",
                                 "/api/auth/login",
                                 "/api/auth/dev/access-token",
-                                "/api/dropdown/*",
+                                "/api/dropdown/**",
                                 "/api/auth/username",
                                 "/api/auth/signup",
                                 "/api/auth/logout",
